450897c07656d5e8d57513a5295626773f97c3e7
bpeak <bpeakcpeak@gmail.com>
2018-11-08 15:21:14 +0900

    v 2.1.0 https 적용

--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,45 @@ global.__rootDir = path.resolve(__dirname)
 
 import dbLauncher from '~db/dbLauncher'
 import ExpressApp from './ExpressApp'
+import { httpsConfig } from '~configs/secret/secret.config.js'
 
 dbLauncher()
 const app = new ExpressApp().app
 
-const PORT = 80
-app.listen(PORT, () => {
-    console.log(`PORT ${PORT} CONNECTED SUCCESS`)
-})
+if(process.env.NODE_ENV === 'development'){
+    const PORT = 80
+    app.listen(PORT, () => {
+        console.log(`DEVSERVER : PORT ${PORT} CONNECTED SUCCESS`)
+    })
+} else {
+    const lex = require('greenlock-express').create({
+        configDir: httpsConfig.configDir,
+        version : httpsConfig.version,
+        server : httpsConfig.server,
+        approveDomains: (opts, certs, cb) => {
+            if (certs) {
+                opts.domains = ['www.bpeakblog.com', 'bpeakblog.com']
+            } else {
+                opts.email = httpsConfig.email
+                opts.agreeTos = true
+            }
+            cb(null, { options: opts, certs })
+        },
+        renewWithin: 81 * 24 * 60 * 60 * 1000,
+        renewBy: 80 * 24 * 60 * 60 * 1000,
+    })
+
+    // handles acme-challenge and redirects to https
+    const PORT_HTTP = 80
+    const PORT_HTTPS = 443
+    require('http').createServer(lex.middleware(require('redirect-https')())).listen(PORT_HTTP, () => {
+        console.log(`PORT ${PORT_HTTP} CONNECTED SUCCESS`)
+    })
+    
+    // handles your app
+    require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(PORT_HTTPS, () => {
+        console.log(`PORT ${PORT_HTTPS} CONNECTED SUCCESS`)
+    })
+}
+
+
